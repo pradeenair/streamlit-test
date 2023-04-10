cc12d90ef4a17d9b6a82884a3617c3de476791b7
pradeenair <120032448+pradeenair@users.noreply.github.com>
2023-04-10 19:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,49 @@
+import os
 import openai
+import streamlit as st
 import google.auth
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-import streamlit as st
+import json
 
-# Google Sheets API setup
+# Set up API credentials
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-
-# Enter your Google Sheet ID and range
 SPREADSHEET_ID = "1y1M1nzulCTWOSPiZAGh_zVeAHwZ6Z8bgwfY0AvIO4Dk"
-RANGE_NAME = "Sheet1!A1:A"  # Adjust according to the column containing the company names
+RANGE_NAME = "Sheet1!A:A"
 
-# ChatGPT API setup
-openai.api_key = "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"
+# Set up OpenAI API
+openai.api_key = os.environ["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"]
 
 
 def get_google_sheet_data(spreadsheet_id, range_name):
     creds = None
+    if "GOOGLE_APPLICATION_CREDENTIALS" in os.environ:
+        creds = Credentials.from_authorized_user_file(os.environ["GOOGLE_APPLICATION_CREDENTIALS"], SCOPES)
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
 
     service = build("sheets", "v4", credentials=creds)
-    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+
+    result = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=spreadsheet_id, range=range_name)
+        .execute()
+    )
     return result.get("values", [])
 
 
-def is_company_in_fortune_1000(company):
-    prompt = f"Is {company} in the Fortune 1000 list?"
+def check_fortune_1000(company_name):
+    prompt = f"Is {company_name} in the Fortune 1000 list?"
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -42,26 +53,22 @@ def is_company_in_fortune_1000(company):
         temperature=0.5,
     )
 
-    answer = response.choices[0].text.strip().lower()
-    return answer == "yes"
+    answer = response.choices[0].text.strip()
+    return answer.lower() == "yes"
 
 
 def main():
     st.title("Fortune 1000 Company Lookup")
-
     companies = get_google_sheet_data(SPREADSHEET_ID, RANGE_NAME)
-    result_list = []
 
+    st.write("### List of Companies:")
     for company in companies:
         company_name = company[0]
-        in_fortune_1000 = is_company_in_fortune_1000(company_name)
-        result_list.append((company_name, in_fortune_1000))
-
-    st.write("Results:")
-
-    for result in result_list:
-        st.write(f"{result[0]}: {'Yes' if result[1] else 'No'}")
+        is_fortune_1000 = check_fortune_1000(company_name)
+        status = "Yes" if is_fortune_1000 else "No"
+        st.write(f"{company_name}: {status}")
 
 
 if __name__ == "__main__":
     main()
+
